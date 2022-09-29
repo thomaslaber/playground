7c7c22a2360bdcecc7ac68664bf09b9fd81be7d0
Thomas Laber <laber.thomas@gmail.com>
2022-09-29 19:26:05 +0200

    corrections

--- art/art.R
+++ art/art.R
@@ -1,6 +1,10 @@
 library(generativeart) # devtools::install_github("cutterkom/generativeart")
 library(ambient)
-library(dplyr)# set the paths
+library(dplyr)
+
+# set the paths
+setwd("/Users/thomas/projects/playground/art")
+
 IMG_DIR <- "img/"
 IMG_SUBDIR <- "everything/"
 IMG_SUBDIR2 <- "handpicked/"
@@ -9,18 +13,23 @@ IMG_PATH <- paste0(IMG_DIR,
 LOGFILE_DIR <- "logfile/"
 LOGFILE <- "logfile.csv"
 LOGFILE_PATH <- paste0(LOGFILE_DIR, 
-                       LOGFILE)# create the directory structure
+                       LOGFILE)
+
+# create the directory structure
 generativeart::setup_directories(IMG_DIR, 
                                  IMG_SUBDIR, 
                                  IMG_SUBDIR2, 
                                  LOGFILE_DIR)
+
 # include a specific formula, for example:
 my_formula <- list(
   x = quote(runif(1, -1, 10) * x_i^2 - sin(y_i^2)),
   y = quote(runif(1, -1, 10) * y_i^3 - cos(x_i^2) * y_i^5)
-)# call the main function to create five images with a polar coordinate system
+)
+
+# call the main function to create five images with a polar coordinate system
 generativeart::generate_img(formula = my_formula, 
-                            nr_of_img = 5, # set the number of iterations
+                            nr_of_img = 1, # set the number of iterations
                             polar = TRUE, 
                             filetype = "png", 
                             color = "#c1a06e", 
